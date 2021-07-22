62836df0013544b257113e220cab9ae35d9d604d
Sam Ryan <r.sam.ryan@gmail.com>
2021-07-22 14:48:04 -0400

    Initial Commit

---
+++ PDFc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDFc
+//
+//  Created by Sam Ryan on 2021-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDFc/PDFcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFcApp.swift
+//  PDFc
+//
+//  Created by Sam Ryan on 2021-07-22.
+//
+
+import SwiftUI
+
+@main
+struct PDFcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
